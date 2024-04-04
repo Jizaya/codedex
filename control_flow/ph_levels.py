be55fe3162948cab0e8f0707402841c47e46760c
Jizaya <104530606+Jizaya@users.noreply.github.com>
2024-04-04 03:35:46 -0700

    Create ph_levels.py

---
+++ control_flow/ph_levels.py
@@ -0,0 +1,8 @@
+#Create a ph_levels.py program that checks whether a pH level is basic, acidic, or neutral.
+ph = 7
+if ph > 7:
+  print("Basic")
+elif ph < 7:
+  print("Acidic")
+else:
+  print("Neutral")
