237df60db6f9ba3d0efe1201f2a343075ae09c20
Jizaya <104530606+Jizaya@users.noreply.github.com>
2024-04-04 03:33:25 -0700

    Create hypotenuse.py

---
+++ variables/hypotenuse.py
@@ -0,0 +1,5 @@
+#Create a hypotenuse.py program that asks the user for two numbers, a and b, and then calculates the hypotenuse c.
+a = int(input("Enter short side"))
+b = int(input("Enter short side"))
+c = ((a ** 2) + (b ** 2)) ** 0.5
+print(c)
