c1a621a245c7005a5373ae2269db17673ffbbdf0
Jizaya <104530606+Jizaya@users.noreply.github.com>
2024-04-04 16:08:26 -0700

    Update ph_levels.py that asks for user input

--- control_flow/ph_levels.py
+++ control_flow/ph_levels.py
@@ -6,3 +6,12 @@ elif ph < 7:
   print("Acidic")
 else:
   print("Neutral")
+
+#Updated ph_levels.py that asks for user input
+ph = int(input('What is the ph? '))
+if ph > 7:
+  print("Basic")
+elif ph < 7:
+  print("Acidic")
+else:
+  print("Neutral")
