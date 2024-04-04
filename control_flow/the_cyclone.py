f95309ed369d1fabf7abbab99eefc629a065bfe7
Jizaya <104530606+Jizaya@users.noreply.github.com>
2024-04-04 16:06:05 -0700

    Update the_cyclone.py to ask user for input

--- control_flow/the_cyclone.py
+++ control_flow/the_cyclone.py
@@ -8,3 +8,15 @@ elif height > 137 and credits < 10:
   print('You do not have enought credits.')
 if height < 137 and credits < 10:
   print('You do not meet either requirement.') 
+
+#Updated Cyclone that asks user for input
+height = int(input('How tall are you in inches? '))
+credits = int(input('How many credits do you have? '))
+if height > 54 and credits > 10:
+  print("Enjoy the ride!")
+elif height < 54 and credits > 10:
+  print('You are not tall enough to ride.')
+elif height > 54 and credits < 10:
+  print('You do not have enought credits.')
+if height < 54 and credits < 10:
+  print('You do not meet either requirement.') 
