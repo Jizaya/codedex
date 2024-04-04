66707494d21d9af483bd7f0e0a5ef590fd6a9930
Jizaya <104530606+Jizaya@users.noreply.github.com>
2024-04-04 03:19:48 -0700

    Create letter.py

---
+++ letter.py
@@ -0,0 +1,11 @@
+#print the following
+#1. Today's date.
+#2. How you are feeling right now.
+#3. What you want to accomplish by learning to code.
+#4. A little message to your older, wiser, and programmer self.
+#5. Your favorite emoji to spice things up!
+print("April 1, 2024")
+print("I am feeling underwhelmed.")
+print("By learning to code, I hope to make myself more marketable for jobs.")
+print("Do everything you dream.")
+print("\U0001F62D")
